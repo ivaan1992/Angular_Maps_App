b00479892f6d8157afea91b8fba5687b8f6c6619
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-31 20:47:00 -0500

    Create links buttons in menu section

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuSection {
+  name: string;
+  route: string;
+}
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styles: [
+  `
+    li {
+      cursor: pointer;
+    }
+  `
   ]
 })
-export class MenuComponent implements OnInit {
 
-  constructor() { }
+export class MenuComponent {
 
-  ngOnInit(): void {
-  }
+  menuItem: MenuSection[] = [
+
+    {
+      name: 'FullScreen',
+      route: '/maps/fullscreen'
+    },
+    {
+      name: 'Zoom Range',
+      route: '/maps/zoom-range'
+    },
+    {
+      name: 'Markers',
+      route: '/maps/markers'
+    },
+    {
+      name: 'Estates',
+      route: '/maps/estates'
+    },
+
+  ]
 
 }
