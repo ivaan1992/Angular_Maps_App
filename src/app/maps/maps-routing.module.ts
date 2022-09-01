919cfec3da89e2f7cb46338663b9cf441d02a844
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-31 20:29:39 -0500

    Create routes and lazyloads to navigate in maps app

--- src/app/maps/maps-routing.module.ts
+++ src/app/maps/maps-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EstatesComponent } from './pages/estates/estates.component';
 
-const routes: Routes = [];
+//Components
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { MarkersComponent } from './pages/markers/markers.component';
+import { ZoomRangeComponent } from './pages/zoom-range/zoom-range.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    children: [
+      {
+        path: 'fullscreen',
+        component: FullScreenComponent
+      },
+      {
+        path: 'zoom-range',
+        component: ZoomRangeComponent
+      },
+      {
+        path: 'markers',
+        component: MarkersComponent
+      },
+      {
+        path: 'estates',
+        component: EstatesComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'fullscreen'
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
