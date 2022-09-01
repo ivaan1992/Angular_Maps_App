a7592a8735c2c7279151a8e27f23554bd0559759
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-31 22:00:44 -0500

    Create a full map in the screen

--- src/app/maps/pages/full-screen/full-screen.component.ts
+++ src/app/maps/pages/full-screen/full-screen.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl'
+import { environment } from '../../../../environments/environment.prod';
 
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
   styles: [
+    `
+    #map {
+      width: 100%;
+      height: 100%;
+    }
+
+    `
+
+
   ]
 })
 export class FullScreenComponent implements OnInit {
@@ -11,6 +22,14 @@ export class FullScreenComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    (mapboxgl as any).accessToken = environment.mapboxToken;
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11'
+    });
+
+
   }
 
 }
